b481c7e95beadfe2430ca0ad40cbfd6870b82a65
Serendo <Serendo@appledeMacBook-Air.local>
2018-07-02 21:54:01 +0800

    hhkb

--- keyboards/bface/config.h
+++ keyboards/bface/config.h
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0x20A0
 #define PRODUCT_ID      0x422D
 #define MANUFACTURER    winkeyless.kr
-#define PRODUCT         B.mini
+#define PRODUCT         B.Face
 
 #define RGBLED_NUM 16
 
